31c1f8e73059673f919ba3acdfbc5299f6f42058
espeonten <sydenteo@gmail.com>
2025-03-31 12:59:30 -0700

    Add files via upload
    added base class and subclasses

---
+++ box.js
@@ -0,0 +1,6 @@
+class Box extends Base{
+    constructor(x, y){
+        super(x, y, 75, 75)
+        this.image = loadImage("sprites/wood1.png")
+    }
+}
---
+++ log.js
@@ -0,0 +1,6 @@
+class Log extends Base{
+    constructor(x,y,width){
+        super(x, y, width, 20)
+        this.image = loadImage("sprites/wood2.png")
+    }
+}
